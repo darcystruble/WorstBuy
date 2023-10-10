3559ad457a36a3b58a34f8d23003e1ba41bbad0e
Josh Hutchison <joshhutchison@hotmail.com>
2023-10-10 16:05:37 -0400

    added some routes for CRUD, added name to options schema, added the npm dependencies and gitignore

--- server.js
+++ server.js
@@ -12,6 +12,27 @@ const app = express();
 app.use(logger('dev'))
 
 
-app.get('/', modelController.getAllModels)
+//cRud - Read
+app.get('/models', modelController.getAllModels)
+app.get('/models/:id', modelController.getOneModel)
+app.get('/makes', makeController.getAllMakes)
+app.get('/makes/:id', makeController.getOneMake)
+app.get('/options', optionsController.getAllOptions)
+app.get('/options/:id', optionsController.getOneOption)
+
+//Crud - Create
+// app.post('/models/', modelController.createModel)
+// app.post('/make/', makeController.createMake)
+// app.post('/options/', optionsController.createOptions)
+
+//crUd - Update
+// app.put('/models/:id', modelController.updateModel)
+// app.put('/make/:id', makeController.updateMake)
+// app.put('/options/:id', optionsController.updateOption)
+
+//cruD - Delete
+// app.delete('/models/:id', modelController.deleteModel)
+// app.delete('/makes/:id', makeController.deleteMake)
+// app.delete('/options/:id', optionsController.deleteOption)
 
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`))
