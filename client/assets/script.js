083fd67139a9c459a43c7dc185ba970f08c7a29a
darcystruble <darcyhallstruble@gmail.com>
2023-10-10 21:39:08 -0400

    javascript updated with basic layout for cars

--- client/assets/script.js
+++ client/assets/script.js
@@ -4,6 +4,23 @@ let searchBar = document.querySelector('.search-bar')
 let searchButton = document.querySelector('.search-button')
 let mainBody = document.querySelector('.main-body')
 
+let carText = `<div class="item-holder">
+<img src="" alt="" class="car-pic">
+<h3 class="car-name">hi</h3>
+<p class="price">price</p>
+</div>`
+
+const clearMainBody = () => {
+    mainBody.innerHTML = ''
+}
+
+searchButton.addEventListener('click', () => {
+    // clearMainBody()
+    console.log('hi')
+    mainBody.innerHTML = carText
+})
+
+
 // HAMBURGER MENU
 const hamburger = document.querySelector('.hamburger')
 const navMenu = document.querySelector('.nav-menu')
@@ -21,9 +38,3 @@ const closeMenu = () => {
 
 hamburger.addEventListener('click', mobileMenu)
 navLink.forEach(link => link.addEventListener('click', closeMenu))
-
-let newText = '<p>HELLO!!</p>'
-
-searchButton.addEventListener('click', () => {
-    console.log('hi')
-})
